249a1cbe3c96409c316089e7fbb55fb820d1ced0
elianaeriko <eliana.eriko@gmail.com>
2022-01-06 11:57:34 -0300

    Versão inicial

---
+++ Peanut_tech.py
@@ -0,0 +1,9 @@
+# PEANUT TECH
+# 01/10/2021
+# Aula Prof. Glauco de Souza Rolim
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+dados = pd.read_excel('https://github.com/GlaucoRolim/dados_publicos/blob/main/dados-ciclo_aguas-amendoim.xlsx?raw=true')
+print("testando")
